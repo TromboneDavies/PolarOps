880dcc57f32e5822397d79bf2847beb6d70a73dc
Stephen <stephen@umw.edu>
2021-06-08 20:16:21 -0400

    Add new data entry scripts.

---
+++ HandTag/completeTagger.py
@@ -0,0 +1,72 @@
+
+# IMPORTANT!! Read the comments at the top of piggyBackTagger.py before running
+# this file!
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats
+import seaborn as sns
+
+import sys
+import os
+
+#Returns a string of all the entire thread of a comment
+def get_thread(top_level_comment, tab, final):
+    first = True
+    if hasattr(top_level_comment, "body"):
+        lines = top_level_comment.body.split("\n")
+        for line in lines:
+
+            for i in range(tab):
+                final = final + "\t"
+
+            if first:
+                final = final + ">"
+                first = False
+
+            final = final + line + "\n"
+
+        for comment in top_level_comment._replies:
+            final = get_thread(comment, tab + 1, final)
+
+    return final
+
+assessors = ["Stephen","TJ","Alexis","Veronica"]
+
+#Ask user questions to prepare for hand tagging
+existing_file = input("What is the name of the existing data file? " +
+    "(Hint: it should start with \"piggied_\".)\n")
+while not os.path.isfile(existing_file):
+    existing_file = input("No such file! Try again:\n")
+preexisting = pd.read_csv(existing_file)
+if not all([ a in preexisting.columns for a in assessors ]):
+    print("Your {} file is jacked up! Quitting now.".format(existing_file))
+    sys.exit(1)
+
+user = input("\nType 1/2/3/4 after each thread to designate which type it is.\n\nYou can type \"exit\" to stop the program.\n\nPlease type your name to begin.\n") 
+
+tt = ""
+legit = ["1", "2", "3", "4"]
+comma = ","
+
+new_assessments = np.repeat(-99,len(preexisting))
+
+for i,thread in enumerate(preexisting.itertuples()):
+    print(thread.text.replace("\\n","\n"))
+    if preexisting.iloc[i][user] != -99:
+        print("You already assessed this as {}.".format(preexisting.iloc[i][user]))
+        new_assessments[i] = preexisting.iloc[i][user]
+    else:
+        tt = input("What thread type is thread {}/{}, {}? (1,2,3,4,exit)".
+            format(i+1,len(preexisting),user))
+        if tt == "exit":
+            break
+        new_assessments[i] = tt
+
+ 
+preexisting[user] = new_assessments
+
+preexisting.to_csv(existing_file, index=None)
+print("Wrote output to {}!".format(existing_file))
+
---
+++ HandTag/piggyBackTagger.py
@@ -0,0 +1,90 @@
+
+# IMPORTANT!! Only run this file when you have an "old-style" (pre June 8) .csv
+# file, with only one assessor's ratings in it. This will produce a new file
+# called "piggied_{oldfilename.csv}" that has four columns for each assessor.
+#
+# Put another way, only run this file when you are the SECOND person to add
+# your thread assessments to a group of threads.
+#
+# If you are the THIRD or FOURTH person to add your thread assessments to a
+# group of threads, you should run "completeTagger.py" instead of this file.
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats
+import seaborn as sns
+
+import os
+
+#Returns a string of all the entire thread of a comment
+def get_thread(top_level_comment, tab, final):
+    first = True
+    if hasattr(top_level_comment, "body"):
+        lines = top_level_comment.body.split("\n")
+        for line in lines:
+
+            for i in range(tab):
+                final = final + "\t"
+
+            if first:
+                final = final + ">"
+                first = False
+
+            final = final + line + "\n"
+
+        for comment in top_level_comment._replies:
+            final = get_thread(comment, tab + 1, final)
+
+    return final
+
+#Ask user questions to prepare for hand tagging
+existing_file = input("What is the name of the existing data file?" +
+    "(Hint: it should NOT begin with \"piggied_\".)\n")
+while not os.path.isfile(existing_file):
+    existing_file = input("No such file! Try again:\n")
+preexisting = pd.read_csv(existing_file, header=None)
+old_assessor = preexisting.iloc[0][5]
+preexisting.columns = ['Subreddit','SubmissionID','CommentID','text','ttype',
+    'assessor','date']
+
+user = input("\nType 1/2/3/4 after each thread to designate which type it is.\n\nYou can type \"exit\" to stop the program.\n\nPlease type your name to begin.\n") 
+
+assessors = ["Stephen","TJ","Alexis","Veronica"]
+tt = ""
+legit = ["1", "2", "3", "4"]
+comma = ","
+
+subreddits = preexisting.Subreddit
+submissionIDs = preexisting.SubmissionID
+commentIDs = preexisting.CommentID
+texts = preexisting.text
+ttypes = { name : np.repeat(-99,len(preexisting)) for name in assessors }
+dates = preexisting.date
+
+for i,thread in enumerate(preexisting.itertuples()):
+    print(thread.text.replace("\\n","\n"))
+    tt = input("What thread type is thread {}/{}, {}? (1,2,3,4,exit)".format(
+        i+1,len(preexisting),user))
+    if tt == "exit":
+        break
+
+    subreddits[i] = preexisting.iloc[i].Subreddit
+    submissionIDs[i] = preexisting.iloc[i].SubmissionID
+    commentIDs[i] = preexisting.iloc[i].CommentID
+    texts[i] = preexisting.iloc[i].text
+    ttypes[old_assessor][i] = preexisting.iloc[i].ttype
+    ttypes[user][i] = tt
+    dates[i] = preexisting.iloc[i].date
+ 
+columns = { 'Subreddit': subreddits,
+    'SubmissionID': submissionIDs,
+    'CommentID': commentIDs,
+    'text': texts,
+    'date': dates }
+columns.update(ttypes)
+
+newguy = pd.DataFrame(columns)
+newguy.to_csv("piggied_{}".format(existing_file), index=None)
+print("Wrote output to piggied_{}!".format(existing_file))
+
