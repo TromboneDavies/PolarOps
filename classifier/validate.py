7e9ea41092cbb0115f683e2d5a571bfb3e419212
Stephen <stephen@umw.edu>
2021-07-17 20:09:54 -0400

    Refactor. Add get_classifier() and perform_cross_validation().

--- classifier/validate.py
+++ classifier/validate.py
@@ -1,14 +1,7 @@
-# prepare.py -- Perform basic data cleansing and model creation in preparation
-# for downstream script like validate.py, cross_validate.py, or interactive.py.
-#
-# When this script is finished running, the following variables will be
-# available to downstream scripts:
-#
-# - define_model()
-# - all_threads
-# - yall
-# - vocab
-# - numWords
+# validate.py: contains functions to create and train a neural net on
+# hand_tagged_data.csv, or some subset thereof, and evaluate its performance.
+# Functions that may be of use outside this script include validate(),
+# validation_hist(), perform_cross_validation(), and get_classifier().
 
 import string
 import re
@@ -43,24 +36,10 @@ WORD_EMBEDDINGS_FILE = "glove.6B.100d.w2v.txt"
 
 
 
-if REMOVE_STOPWORDS:
-    stop = [ remove_punct(sw) for sw in stopwords.words('english') ]
-else:
-    stop = []
 
-if EMBEDDINGS:
-    print("Loading word embeddings...")
-    binary = False if WORD_EMBEDDINGS_FILE.endswith(".txt") else True
-    wv = KeyedVectors.load_word2vec_format(WORD_EMBEDDINGS_FILE, binary=binary)
-    print("...done.")
-
-
-
-# SD
 def tokenize(thread):
     return [ w for w in thread.split(" ") if w not in stop ]
     
-# orig/SD
 def thread_to_tokens(thread, vocab=None):
     tokens = tokenize(remove_punct(thread))
     if vocab:
@@ -75,17 +54,14 @@ def process_threads(df, vocab):
         lines.append(thread_to_tokens(row.text, vocab))
     return lines
 
-# orig/SD
 def load_clean_dataset(df, vocab):
     return process_threads(df, vocab), np.where(df.polarized=="yes",1,0)
 
-# orig
 def create_tokenizer(lines):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(lines)
     return tokenizer
 
-# orig - define the model
 def define_model(numWords):
     # define network
     model = Sequential()
@@ -99,7 +75,7 @@ def define_model(numWords):
     #plot_model(model, to_file='model.png', show_shapes=True)
     return model
 
-# orig/SD - classify a thread as polarized or not polarized
+
 def predict_polarized(thread, vocab, tokenizer, model):
     tokens = thread_to_tokens(thread)
     # unneeded?  tokens = [w for w in tokens if w in vocab]
@@ -114,7 +90,7 @@ def predict_polarized(thread, vocab, tokenizer, model):
         return (1-percent_polar), 'no'
     return percent_polar, 'yes'
 
-# SD - compute vocabulary
+
 def compute_vocab(df, numWords):
     vocab = Counter()
     for row in df.itertuples():
@@ -122,7 +98,8 @@ def compute_vocab(df, numWords):
     return [ w for w,c in vocab.most_common(numWords) 
         if w not in ['newcomment','inthreadquote'] ]
 
-# SD - compute the centroid (Stephen's "dumb idea")
+
+# Compute the centroid (Stephen's "dumb idea")
 def embed(threads):
     ignored = set()
     mat = np.zeros(shape=(len(threads),wv.vector_size))
@@ -142,53 +119,61 @@ def embed(threads):
     return mat
         
 
-# load and shuffle the training data
-df = pd.read_csv("hand_tagged_data.csv")
-df = df.sample(frac=1)
-vocab = compute_vocab(df,NUM_TOP_WORDS)
 
-
-## load all threads and labels.
-all_threads, yall = load_clean_dataset(df, vocab)
-if EMBEDDINGS:
-    allX = embed(all_threads)
-    numWords = allX.shape[1]
-else:
-    # create the tokenizer, for use in calling .texts_to_matrix().
-    tokenizer = create_tokenizer(all_threads)
-    # encode data
-    allX = tokenizer.texts_to_matrix(all_threads, mode=METHOD)
-numWords = allX.shape[1]
+# Given the training data passed, return a neural net on which .predict() can
+# be called to make predictions on new data. (The argument to .predict() should
+# be a list of strings, each of which is a single thread.)
+def get_classifier(threads, labels):
+    model = define_model(numWords)
+    Xtrain = encode_features(threads)
+    model.fit(Xtrain, labels, epochs=NUM_EPOCHS, verbose=0)
+    return model
 
 
 
-def validate(all_threads, yall, test_size=.2):
+# Given a set of threads, return a matrix of features.
+def encode_features(threads):
+    if EMBEDDINGS:
+        return embed(threads)
+    else:
+        return tokenizer.texts_to_matrix(threads, mode=METHOD)
+
+
+# Train the model one time on a randomly-chosen training set from the data set
+# passed, and return the correctness of its predictions on all elements of the
+# remaining data points (i.e., the test set).
+# "all_threads" contains the features, and "yall" contains the labels.
+def validate(all_threads, yall, test_frac=.2):
 
     # split into training and test set
     training_threads, test_threads, ytrain, ytest = train_test_split(
-        all_threads, yall, test_size=test_size)
+        all_threads, yall, test_size=test_frac)
 
     # encode separate training and test matrices
-    if EMBEDDINGS:
-        Xtrain = embed(training_threads)
-        Xtest = embed(test_threads)
-    else:
-        Xtrain = tokenizer.texts_to_matrix(training_threads, mode=METHOD)
-        Xtest = tokenizer.texts_to_matrix(test_threads, mode=METHOD)
-
-    model = define_model(numWords)
-    histo = model.fit(Xtrain, ytrain, epochs=NUM_EPOCHS, verbose=0)
+    model = get_classifier(training_threads, ytrain)
+    Xtest = encode_features(test_threads)
 
     return model.predict(Xtest)[:,0].round() == ytest
 
 
 
-def validation_hist(numModels=100,title=""):
+# Train the model on numModels randomly-chosen training sets, and return an
+# array of the models' accuracies (as a percentage).
+def perform_cross_validation(numModels,all_threads,yall,test_frac=.2):
     accuracies = np.empty(numModels)
     for i in range(numModels):
         print("\nTraining model {}/{}...".format(i+1,numModels))
-        results = validate(all_threads, yall)
+        results = validate(all_threads, yall, test_frac)
         accuracies[i] = sum(results)/len(results)*100
+    return accuracies
+
+
+
+# Train the model on numModels randomly-chosen training sets, and plot a
+# histogram of the models' accuracies (as a percentage). The filename of the
+# plot will be a concatenation of the model parameters, followed by ".png".
+def validation_hist(numModels=100,title=""):
+    accuracies = perform_cross_validation(numModels,all_threads,yall)
     plt.clf()
     pd.Series(accuracies).hist(density=True, bins=range(0,100,4))
     ax = plt.gca()
@@ -207,7 +192,6 @@ def validation_hist(numModels=100,title=""):
     title = title if len(title) > 0 else def_title
     plt.title(title)
     plt.savefig(title.replace(" ","_") + ".png")
-    return accuracies
         
 
 def validate_one():
@@ -217,4 +201,36 @@ def validate_one():
 
 
 
-validation_hist(100)
+
+if REMOVE_STOPWORDS:
+    stop = [ remove_punct(sw) for sw in stopwords.words('english') ]
+else:
+    stop = []
+
+if EMBEDDINGS:
+    print("Loading word embeddings...")
+    binary = False if WORD_EMBEDDINGS_FILE.endswith(".txt") else True
+    wv = KeyedVectors.load_word2vec_format(WORD_EMBEDDINGS_FILE, binary=binary)
+    print("...done.")
+
+
+# Load and shuffle the training data
+df = pd.read_csv("hand_tagged_data.csv")
+df = df.sample(frac=1)
+vocab = compute_vocab(df,NUM_TOP_WORDS)
+
+
+# Load all threads and labels.
+all_threads, yall = load_clean_dataset(df, vocab)
+if EMBEDDINGS:
+    allX = embed(all_threads)
+    numWords = allX.shape[1]
+else:
+    # create the tokenizer, for use in calling .texts_to_matrix().
+    tokenizer = create_tokenizer(all_threads)
+    # encode data
+    allX = tokenizer.texts_to_matrix(all_threads, mode=METHOD)
+numWords = allX.shape[1]
+
+
+#validation_hist(100)
