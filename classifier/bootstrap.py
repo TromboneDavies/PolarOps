32a6ddcee4be92e2081da8bb31ff281def0415c2
Stephen <stephen@umw.edu>
2021-07-19 12:08:33 -0400

    Fix bug with categorical vs. numeric polarization labels.

--- classifier/bootstrap.py
+++ classifier/bootstrap.py
@@ -16,7 +16,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 import datetime as dt
 import tensorflow.compat.v1.logging
-from polarops import create_model, create_vectorizer, remove_punct, get_features
+from polarops import create_model, create_vectorizer, remove_punct
+from polarops import get_features, stem
 import sys
 from validate import get_classifier, perform_cross_validation, encode_features
 
@@ -25,7 +26,7 @@ from validate import get_classifier, perform_cross_validation, encode_features
 
 # The number of new (unlabeled) data points to use in each iteration of the
 # bootstrap process.
-BOOTSTRAP_SIZE = 100
+BOOTSTRAP_SIZE = 10000
 
 # The number of random training sets to use for each model cross-validation.
 NUM_MODELS = 20
@@ -34,8 +35,8 @@ NUM_MODELS = 20
 # to be "safe" to use. If its prediction is < min_bound, we consider it safely
 # non-polarized. If its prediction is > max_bound, we consider it safely
 # polarized. Otherwise, we consider it unreliable and won't use it.
-min_bound = .1
-max_bound = .95
+min_bound = .01
+max_bound = .99
 
 # int: number of most common words/bigrams to retain
 numTopFeatures = 6000
@@ -56,22 +57,25 @@ removeStopwords = False
 useBigrams = True
 
 # bool: Use number of comments as a feature
-comments = True
+comments = False
 
 # bool: Use average number of in thread quotes as a feature
-itquotes = True
+itquotes = False
 
 # bool: Use average number of links as a feature
-links = True
+links = False
 
 # bool: Use average word length as a feature
-wordLength = True
+wordLength = False
 
 # bool: Use lexical diversity as a feature
 ld = True
 
+# bool: Stem words, or use raw words?
+stem = True
+
 # float: ignore unigrams/bigrams with document frequency higher than this
-maxDf = .9
+maxDf = .95
 
 ############################################################################
 
@@ -117,10 +121,8 @@ print("\n    Starting bootstrapping...")
 loop_num = 0
 while previousAccuracy <= currAccuracy:
 
-    # TJ makes another good point: what if we run out of the bootstrap
-    # reservoir before we go down in accuracy? Yeah, T, handle that too.
-
     if len(bootstrapReservoir) < BOOTSTRAP_SIZE:
+        previousAccuracy = currAccuracy
         print("No more bootstrap data!")
         break
     bootstrap = bootstrapReservoir.sample(BOOTSTRAP_SIZE)
@@ -134,11 +136,12 @@ while previousAccuracy <= currAccuracy:
     predictions = bootstrapResults[(bootstrapResults < min_bound) | 
         (bootstrapResults > max_bound)]
     
-    toKeep['polarized'] = np.where(predictions < .5, 0, 1)
+    toKeep['polarized'] = np.where(predictions < .5, "no", "yes")
     handTaggedPlus = handTaggedPlus.append(toKeep)
 
     trainingData = np.append(trainingData, toKeep.text)
-    trainingLabels = np.append(trainingLabels, toKeep['polarized'])
+    trainingLabels = np.append(trainingLabels, 
+        np.where(toKeep['polarized'] == "no", 0, 1))
 
     previousAccuracy = currAccuracy
     currAccuracy = perform_cross_validation(NUM_MODELS, trainingData,
@@ -146,11 +149,14 @@ while previousAccuracy <= currAccuracy:
     if previousAccuracy > currAccuracy:
         handTaggedPlus = handTaggedPlus.drop(toKeep.index)
     else:
-        print(("\n    We just added {} samples, and went from {:.2f}% to " +
-            "{:.2f}% accuracy.").format(len(toKeep), previousAccuracy,
-            currAccuracy))
+        print(("\n    We just added {} samples ({} nonpolar and {} polar)"+
+            ", and went from {:.2f}% to " + "{:.2f}% accuracy.").format(
+            len(toKeep), sum(toKeep['polarized'] == "no"),
+            sum(toKeep['polarized'] == "yes"), previousAccuracy, currAccuracy))
     loop_num+= 1
 
+print("\n    The final accuracy, after {} bootstrap iterations, was {:.2f}%".
+    format(loop_num, previousAccuracy))
 
 # Finally, write our hand-tagged training data, with all our accuracy-
 # increasing bootstapped samples, to a file.
