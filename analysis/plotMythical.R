80432f8a7685d3e4f27f8d2fbec3166c08428030
Stephen <stephen@umw.edu>
2021-07-20 20:45:29 -0400

    Plot the Mythical Graph™.

---
+++ analysis/plotMythical.R
@@ -0,0 +1,19 @@
+
+require(ggplot2)
+require(RColorBrewer)
+
+# Giving this file a .txt instead of .csv extension so that git doesn't ignore
+# it (we told it we don't want to commit/push .csv files, since most of ours
+# are so big.)
+mdf <- read.csv("mythicalDataFrame.txt")
+
+p <- ggplot(mdf,
+    aes(x=year, y=perc_polar, color=subreddit, group=subreddit)) +
+    geom_line(aes(color=subreddit), size=1) +
+    scale_color_brewer(palette="Paired") +
+    scale_x_continuous(name="Year", breaks=c(2008:2021)) +
+    ylim(c(-5,105)) +
+    ylab("% of Threads Polarized") +
+    labs(title="Estimated Polarization of Subreddits Over Time") +
+    theme(plot.title=element_text(hjust=0.5))
+ggsave("mythicalGraphBySubreddit.png", height=6, width=9)
