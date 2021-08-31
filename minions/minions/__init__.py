94dcb5672b231cd3ca9d9395a8c330ac08441dbd
Stephen <stephen@umw.edu>
2021-08-30 20:55:13 -0400

    Add login functionality.

--- minions/minions/__init__.py
+++ minions/minions/__init__.py
@@ -1,4 +1,5 @@
 
 from flask import Flask
 collector = Flask(__name__)
+collector.secret_key = b'askfjalkj'
 from minions import routes
--- minions/minions/routes.py
+++ minions/minions/routes.py
@@ -1,5 +1,24 @@
 from minions import collector
+from flask import render_template, session, redirect, request, url_for
 
 @collector.route("/")
-def index():
-    return "Hello, Minions!"
+@collector.route("/collect")
+def collect():
+    if 'name' in session:
+        return f"welcome, {session['name']}!"
+    else:
+        return redirect(url_for("who"))
+
+
+@collector.route("/who", methods=['GET','POST'])
+def who():
+    if request.method == "POST":
+        if request.form['codename'] != "polarops":
+            return """
+            <h1 style="color:red;">JUST WHO DO YOU THINK YOU ARE???</h1>
+            """
+        session['name'] = request.form['name']
+        return redirect(url_for("collect"))
+    else:
+        return render_template("who.html")
+
