c0511335b8cc7e4d4e93a1aae646d066f03e34c2
Stephen <stephen@umw.edu>
2021-08-31 20:27:19 -0400

    Add read-only functionality for SQLite.

--- minions/minions/__init__.py
+++ minions/minions/__init__.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
-collector = Flask(__name__)
-collector.secret_key = b'askfjalkj'
+collector = Flask(__name__, instance_relative_config=True)
+collector.config.from_envvar("SETTINGS_FILE")
+collector.secret_key = collector.config['SECRET_KEY']
 from minions import routes
--- minions/minions/routes.py
+++ minions/minions/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, session, redirect, request, url_for
+from flask import ( render_template, session, redirect, request, url_for,
+    current_app, Markup)
 import sqlite3
+import os
 from minions import collector
 
 
@@ -7,10 +9,33 @@ from minions import collector
 @collector.route("/collect")
 def collect():
     if 'name' in session:
-        conn = sqlite3.connect("pile.db")
-        results = conn.execute("select * from prac").fetchall()
+        conn = sqlite3.connect(os.path.join(collector.instance_path,
+                current_app.config['DATABASE']),
+            detect_types=sqlite3.PARSE_DECLTYPES)
+        conn.execute(
+            f"""
+            drop table if exists {session['name']}
+            """
+        )
+        conn.execute(
+            f"""
+            create temporary table {session['name']} as 
+                select comment_id from pile
+                except select comment_id from rated where rater=?
+            """, (session['name'],)).fetchone()
+        thread_to_rate = conn.execute(
+            f"""
+            select comment_id,text from pile where comment_id=
+            (select comment_id from {session['name']} order by random() limit 1)
+            """).fetchone()
+        num_threads = conn.execute(
+            f"""
+            select count(*) from rated where rater=?
+            """, (session['name'],)).fetchone()[0]
         conn.close()
-        return render_template("tag.html",results=results)
+        thread = thread_to_rate[1].replace("\\n","<br>")
+        return render_template("tag.html",thread_to_rate=Markup(thread),
+            num_threads=num_threads+1)
     else:
         return redirect(url_for("who"))
 
