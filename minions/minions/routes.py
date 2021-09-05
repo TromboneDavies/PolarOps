d89bcf4a9b6ae692f6a4f2fb488d97eafb1a1917
Stephen <stephen@umw.edu>
2021-09-05 17:25:08 -0400

    Update algorithm to increase overlap.

--- minions/minions/routes.py
+++ minions/minions/routes.py
@@ -33,28 +33,64 @@ def collect():
 # Returns a tuple containing the comment ID of a thread, the thread's text, and
 # the number of threads already tagged by this user.
 def select_thread_for(name):
+
+    # First, figure out which pile(s) to use.
+    if name in all_piles_users:
+        pilename = random.choose(['pile1','pile2','pile3'])
+    elif name in pile_assignments:
+        pilename = f"pile{pile_assignments[name]}"
+    else:
+        print(f"Bad username '{name}'!!")
+        return 0
     conn = sqlite3.connect(os.path.join(collector.instance_path,
             current_app.config['DATABASE']),
         detect_types=sqlite3.PARSE_DECLTYPES)
     if name in ['pile','rated']:
         print(f"NO WAY will we allow '{name}' as a name!")
         return 0
+
+    # First, assign this user a thread which other users in this same pile
+    # have rated, if applicable.
     conn.execute(
         f"""
         drop table if exists {name}
         """
     )
-    conn.execute(
-        f"""
-        create temporary table {name} as 
-            select comment_id from pile
-            except select comment_id from rated where rater=?
-        """, (name,)).fetchone()
     thread_to_rate = conn.execute(
         f"""
-        select comment_id,text from pile where comment_id=
-        (select comment_id from {name} order by random() limit 1)
-        """).fetchone()
+        select rated.comment_id, text from
+            rated join `{pilename}`
+            on rated.comment_id=`{pilename}`.comment_id
+            where rated.comment_id not in
+            (select comment_id from rated where rater=?)
+        order by random() limit 1
+        """, (name,)).fetchone()
+
+    if not thread_to_rate:
+        # BUT if this user has rated all the threads in this pile that their
+        # co-minions did, pick a fresh one from the pile.
+        conn.execute(
+            f"""
+            create temporary table {name} as
+                select `{pilename}`.comment_id from `{pilename}`
+                except select comment_id from rated where rater=?
+            """, (name,))
+        thread_id_to_rate = conn.execute(
+            f"""
+            select comment_id from {name} order by random() limit 1
+            """).fetchone()[0]
+        thread_to_rate = conn.execute(
+            f"""
+            select comment_id,text from {pilename} where comment_id=?
+            """, (thread_id_to_rate,)).fetchone()
+
+    if not thread_to_rate:
+        # If we reach here, then our father is truly dead.
+        print(f"Absolutely no more threads for minion {name} to rate!")
+        return None
+
+    # In either success case, we're also returning the number of threads this
+    # minion has rated so far.
     num_threads = conn.execute(
         f"""
         select count(*) from rated where rater=?
