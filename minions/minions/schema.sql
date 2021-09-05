fbf41b66485a295b7620c7befd1dc30d5efba289
Stephen <stephen@umw.edu>
2021-09-05 17:29:33 -0400

    Add piles to schema.sql.

--- minions/minions/schema.sql
+++ minions/minions/schema.sql
@@ -1,7 +1,23 @@
 
 CREATE TABLE rated (comment_id text, rater text, rating text);
 
-CREATE TABLE pile(
+CREATE TABLE pile1 (
+  "subreddit" TEXT,
+  "submission_id" TEXT,
+  "comment_id" TEXT,
+  "text" TEXT,
+  "date" TEXT,
+  "batch_num" TEXT
+);
+CREATE TABLE pile2 (
+  "subreddit" TEXT,
+  "submission_id" TEXT,
+  "comment_id" TEXT,
+  "text" TEXT,
+  "date" TEXT,
+  "batch_num" TEXT
+);
+CREATE TABLE pile3 (
   "subreddit" TEXT,
   "submission_id" TEXT,
   "comment_id" TEXT,
