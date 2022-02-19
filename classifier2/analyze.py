ee84dd528fd9b6e9e37cc89d8861be343a558362
Stephen <stephen@umw.edu>
2022-02-19 13:17:08 -0500

    Omit redundant loading.

--- classifier2/analyze.py
+++ classifier2/analyze.py
@@ -3,20 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
-import loadData
+from loadData import ht, vectorize
 
 # See also https://scikit-learn.org/stable/modules/classes.html#text-feature-extraction-ref
 from sklearn.feature_extraction.text import CountVectorizer
 
-ht = pd.read_csv("TheHandTaggedDataBaby.csv", comment="#")
-cleaned_texts = np.empty(len(ht), dtype=object)
-for i,row in enumerate(ht.itertuples()):
-    cleaned_texts[i] = loadData.clean(row.text)
-ht['text'] = cleaned_texts
-
 # Show bar graph of N most common words.
 N = 30
-features, meanings = loadData.vectorize(ht.text)
+features, meanings = vectorize(ht.text)
 
 plt.figure()
 plt.rc('xtick',labelsize=6)
@@ -42,4 +36,5 @@ plt.axvline(x=lengths.median(), color="red", linestyle="dashed")
 plt.annotate(f"mean: {lengths.mean():.1f} chars", (2000,360), color="black")
 plt.annotate(f"median: {lengths.median():.1f} chars", (3000,400), color="red")
 plt.savefig("threadLengths.png", dpi=300)
+
 
