87d4def688385e0d9e7c7a7c957d16c37dcdd631
Stephen <stephen@umw.edu>
2022-02-19 13:38:51 -0500

    Comment, shuffle data, and make more modular.

--- classifier2/loadData.py
+++ classifier2/loadData.py
@@ -1,3 +1,14 @@
+# Load the data from TheHandTaggedDataBaby.csv and prepare for classification:
+#   - Shuffle data
+#   - Split into training and validation sets
+#   - Clean text (case, punctuation, etc.; see clean() function and options to
+#     CountVectorizer constructor in vectorize() function)
+#   - Vectorize according to counts (later: TF/IDF)
+#
+# The following variables are available after running this file:
+#   - ht: the entire shuffled and labeled hand-tagged data set
+#   - train: the subset of ht used for training
+#   - validate: the fraction of ht used for validation
 
 import numpy as np
 import pandas as pd
@@ -10,7 +21,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 random.seed(2022)
 
-TRAIN_FRAC = .8
+TRAIN_FRAC = .8     # Fraction of data used for training (not validation)
+NGRAM_MAX = 1       # 1 - unigrams only; 2- unigrams+bigrams; etc.
 
 
 
@@ -27,8 +39,8 @@ def vectorize(texts):
     vectorizer = CountVectorizer(
         encoding='utf-8',
         lowercase=True,
-        stop_words='english',     # could use 'english'
-        ngram_range=(1,1),
+        stop_words='english',     # could use None
+        ngram_range=(1,NGRAM_MAX),
         analyzer='word',
         min_df=0.0,
         max_df=1.0,
@@ -40,6 +52,7 @@ def vectorize(texts):
 
 
 ht = pd.read_csv("TheHandTaggedDataBaby.csv", comment="#")
+ht.sample(frac=1)   # Shuffle the data before doing anything else.
 
 # Eliminate the top 10% longest threads, since they comprise most of a long,
 # probably-not-representative tail. (See email chain 2/18/2022.)
