1cba85f2d0c6fe92011ef5d1b05b69cf0b816815
Stephen <stephen@umw.edu>
2022-02-19 16:52:55 -0500

    Compute and print top indicative features.

--- classifier2/analyze.py
+++ classifier2/analyze.py
@@ -5,13 +5,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 from loadData import ht, vectorize
+from sklearn.feature_selection import SelectKBest, f_classif
 
 # See also https://scikit-learn.org/stable/modules/classes.html#text-feature-extraction-ref
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Show bar graph of N most common words.
 N = 30
-features, meanings = vectorize(ht.text)
+features, meanings, _ = vectorize(ht.text)
 
 plt.figure()
 plt.rc('xtick',labelsize=6)
@@ -46,3 +47,48 @@ plt.savefig("threadLengths.png", dpi=300)
 # https://developers.google.com/machine-learning/guides/text-classification/step-2-5
 ratio = len(ht) / lengths.sum()
 print(f"The Google ratio is {ratio:.5f}.")
+
+
+# Compute and print the most indicative features. 
+NUM_FEATURES = 50
+features, feature_names, vocab = vectorize(ht.text)
+
+selector = SelectKBest(f_classif, k='all')
+selector.fit(features, ht.polarized)
+feature_names_for_df = selector.get_feature_names_out(feature_names)
+scores_for_df = selector.scores_
+important_features = pd.DataFrame({'feature_name':feature_names_for_df,
+    'score':scores_for_df}).set_index('feature_name')
+important_features = important_features.sort_values("score",
+    ascending=False)[0:NUM_FEATURES]
+
+
+from sklearn.naive_bayes import BernoulliNB
+clf = BernoulliNB()
+clf.fit(features, ht.polarized)
+
+indications = np.empty(NUM_FEATURES, dtype="object")
+for i, feature_name in enumerate(important_features.index[0:NUM_FEATURES]):
+
+    feature_index = np.where(feature_names == feature_name)[0][0]
+
+    # Note this is counting # of occurrences, not just presence/absence...
+    num_polar = sum(features[ht.polarized][:, feature_index])
+    num_nonpolar = sum(features[~ht.polarized][:, feature_index])
+
+    prop_polar = num_polar / sum(ht.polarized)
+    prop_nonpolar = num_nonpolar / sum(~ht.polarized)
+
+    if prop_polar > prop_nonpolar:
+        indications[i] = "polar"
+    elif prop_polar < prop_nonpolar:
+        indications[i] = "nonpolar"
+    else:
+        indications[i] = "tie"
+
+important_features['indication'] = indications
+
+print(f"The {NUM_FEATURES} most indicative features are:")
+important_features = important_features.sort_values("score",
+    ascending=False)[0:NUM_FEATURES]
+print(important_features)
