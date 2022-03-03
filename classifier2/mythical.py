93ee36c4bf4766081cd222a2a40a4fafbddbdade
Stephen <stephen@umw.edu>
2022-03-03 16:33:40 -0500

    Put all subreddits on same master plot.

--- classifier2/mythical.py
+++ classifier2/mythical.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats
 from os.path import exists
-from glob import glob
 
 
 national_subreddits = ['politics', 'conservative', 'progressive', 'democrats',
@@ -20,6 +19,8 @@ state_subreddits = [ 'ArizonaPolitics', 'illinoispolitics',
 print("Reading all_predictions.csv...", flush=True)
 all_pred = pd.read_csv("all_predictions.csv")
 
+plt.tight_layout()
+
 if not exists("all_polar_scores.png"):
     print("Creating all_polar_scores.png...", flush=True)
     plt.clf()
@@ -32,6 +33,7 @@ else:
 if not exists("date_hist.png"):
     print("Creating date_hist.png...", flush=True)
     plt.clf()
+    plt.figure(figsize=(16,10))
     num_bins = (all_pred.year.max() - all_pred.year.min() - 1)*12 + \
         12 - all_pred[all_pred.year==min(all_pred.year)].month.min() + 1 + \
         all_pred[all_pred.year==max(all_pred.year)].month.max()
@@ -48,13 +50,16 @@ else:
 if not exists("mythicalMeanScore.png"):
     print("Creating mythicalMeanScore.png...", flush=True)
     plt.clf()
+    plt.figure(figsize=(12,8))
     all_pred['date'] = all_pred.year + all_pred.month/12
     grouped = all_pred.groupby(['date']).polar_score.mean()
     plt.plot(grouped.index, grouped)
-    plt.title("The Mythical Graph (mean polarization score)")
+    plt.suptitle("The Mythical Graph (mean polarization score)",
+        size=22)
     plt.xlabel("Year/month")
     plt.ylabel("Mean polarization score of all threads")
     plt.ylim((0,.5))
+    plt.tight_layout()
     plt.savefig("mythicalMeanScore.png")
 else:
     print("(Skipping mythicalMeanScore.png.)")
@@ -62,23 +67,25 @@ else:
 if not exists("mythicalPercentPolarized.png"):
     print("Creating mythicalPercentPolarized.png...", flush=True)
     plt.clf()
+    plt.figure(figsize=(12,8))
     all_pred['date'] = all_pred.year + all_pred.month/12
     all_pred['pred'] = (all_pred.polar_score > .5) * 100.0
     grouped = all_pred.groupby(['date']).pred.mean()
     plt.plot(grouped.index, grouped)
-    plt.title("The Mythical Graph (% threads polarized)")
+    plt.suptitle("The Mythical Graph (% threads polarized)",
+        size=22)
     plt.xlabel("Year/month")
     plt.ylabel("Percentage of all threads polarized")
     plt.ylim((0,50))
+    plt.tight_layout()
     plt.savefig("mythicalPercentPolarized.png")
 else:
     print("(Skipping mythicalPercentPolarized.png.)")
 
 
 # Dividing subreddit plots
-if len(glob("mythicalBySubreddit*.png")) < 5:
+if not exists("mythicalBySubreddit.png"):
     print("Creating mythicalBySubreddit.png...", flush=True)
-    plt.clf()
     all_pred['date'] = all_pred.year + all_pred.month/12
     all_pred['pred'] = (all_pred.polar_score > .5) * 100.0
 
@@ -97,23 +104,28 @@ if len(glob("mythicalBySubreddit*.png")) < 5:
         national_subreddits[thirdway_index:twothirdsway_index])]
     nationalC = indexed[indexed.subreddit.isin(
         national_subreddits[twothirdsway_index:])]
-    halfway_index = int(len(state_subreddits)/2)
+    thirdway_index = int(len(state_subreddits)/3)
+    twothirdsway_index = thirdway_index * 2
     stateA = indexed[indexed.subreddit.isin(
-        state_subreddits[:halfway_index])]
+        state_subreddits[:thirdway_index])]
     stateB = indexed[indexed.subreddit.isin(
-        state_subreddits[halfway_index:])]
+        state_subreddits[thirdway_index:twothirdsway_index])]
+    stateC = indexed[indexed.subreddit.isin(
+        state_subreddits[twothirdsway_index:])]
 
-    for group in [ f"national{g}" for g in "ABC" ] + \
-            [ f"state{g}" for g in "AB" ]:
-        plt.clf()
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(24,15), sharex=True)
+    for ax, group in zip(axs.flatten(), [ f"national{g}" for g in "ABC" ] + \
+            [ f"state{g}" for g in "ABC" ]):
         globals()[group].groupby('subreddit')['pred'].plot(
-            legend=True)
-        plt.title("The Mythical Graph by Subreddit (% threads polarized)")
-        plt.xlabel("Year/month")
-        plt.ylabel("Percentage of all threads in Subreddit polarized")
-        plt.ylim((0,100))
-        plt.xlim((2007,2022))
-        plt.savefig(f"mythicalBySubreddit{group}.png")
+            legend=True, ax=ax)
+        plt.suptitle("The Mythical Graph by Subreddit (% threads polarized)",
+            size=28)
+        ax.set_xlabel("Year/month")
+        ax.set_ylabel("Percentage of all threads in Subreddit polarized")
+        ax.set_ylim((0,100))
+        ax.set_xlim((2007,2022))
+    plt.tight_layout()
+    plt.savefig(f"mythicalBySubreddit.png")
 
 else:
     print("(Skipping mythicalBySubreddit.png.)")
