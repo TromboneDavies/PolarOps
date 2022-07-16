f8bb02dd275554407f951d75362140ba13679960
Stephen <stephen@umw.edu>
2022-07-16 16:42:24 -0400

    Print interactive confidences (if possible).

--- classifier2/skpolaropsinteractive.py
+++ classifier2/skpolaropsinteractive.py
@@ -6,10 +6,17 @@ import sys
 if not Path("skpolarops.joblib").is_file():
     sys.exit("No skpolarops.joblib model file.")
 else:
+
     classifier = joblib.load("skpolarops.joblib")
     sent = input("enter a sentence (or 'done'): ")
+
     while sent != 'done':
         pred = "polar" if classifier.predict([sent])[0] else "nonpolar"
         print(f"Prediction: {pred}")
+
+        # Unfortunately, SVG doesn't give prediction probabilities. :(
+        if not any([name == "sgdclassifier" for name, _ in classifier.steps]):
+            conf = classifier.predict_proba([sent]).max()
+            print(f"Confidence: {conf*100:.1f}")
 
         sent = input("enter a sentence (or 'done'): ")
